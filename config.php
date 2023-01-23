154de490d48ca5fe4626e8afbad1a04bf157c5d5
storbel <contact@storbel.com>
2023-01-23 13:01:12 +0100

    Update config.php

--- config.php
+++ config.php
@@ -3,8 +3,11 @@
 $databaseHost = $_ENV['DB_URL'];
 $databaseUsername = $_ENV['DB_USER'];
 $databasePassword = $_ENV['DB_PASSWORD'];
-$databaseName = 'rolodex';
-
+$databaseName = $_ENV['DB_NAME'];
+echo "databaseHost:" . $databaseHost . "<br>";
+echo "databaseUsername:" . $databaseUsername . "<br>";
+echo "databasePassword:" . $databasePassword . "<br>";
+echo "databaseName:" . $databaseName . "<br>";
 // Connect to the database
 $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
 ?>
